8cacb840e0b877252281f25350ede5b8647eea4d
=Keenan Gray <krgray@wpi.edu>
2016-03-26 12:59:20 -0400

    add cardboard sdk to project

--- UProj/Assets/StompyRobot/TypeSafe/usr/Resources.Generated.cs
+++ UProj/Assets/StompyRobot/TypeSafe/usr/Resources.Generated.cs
@@ -21,77 +21,180 @@ public sealed class SRResources {
     
     private const string _tsInternal = "1.2.1-Unity5";
     
-    public static global::TypeSafe.Resource<global::UnityEngine.Shader> SteamVR_Fade {
+    public static global::TypeSafe.Resource<global::UnityEngine.Shader> SkyboxMesh {
         get {
             return ((global::TypeSafe.Resource<global::UnityEngine.Shader>)(__ts_internal_resources[0]));
         }
     }
     
-    public static global::TypeSafe.Resource<global::UnityEngine.Shader> SteamVR_BlitFlip {
+    public static global::TypeSafe.Resource<global::UnityEngine.Shader> SteamVR_Fade {
         get {
             return ((global::TypeSafe.Resource<global::UnityEngine.Shader>)(__ts_internal_resources[1]));
         }
     }
     
-    public static global::TypeSafe.Resource<global::UnityEngine.Shader> SteamVR_ClearAll {
+    public static global::TypeSafe.Resource<global::UnityEngine.Shader> SteamVR_BlitFlip {
         get {
             return ((global::TypeSafe.Resource<global::UnityEngine.Shader>)(__ts_internal_resources[2]));
         }
     }
     
+    public static global::TypeSafe.Resource<global::UnityEngine.Shader> SteamVR_ClearAll {
+        get {
+            return ((global::TypeSafe.Resource<global::UnityEngine.Shader>)(__ts_internal_resources[3]));
+        }
+    }
+    
     public static global::TypeSafe.PrefabResource SteamVR_ExternalCamera {
         get {
-            return ((global::TypeSafe.PrefabResource)(__ts_internal_resources[3]));
+            return ((global::TypeSafe.PrefabResource)(__ts_internal_resources[4]));
         }
     }
     
-    public static global::TypeSafe.Resource<global::UnityEngine.Shader> SteamVR_HiddenArea {
-        get {
-            return ((global::TypeSafe.Resource<global::UnityEngine.Shader>)(__ts_internal_resources[4]));
-        }
-    }
-    
-    public static global::TypeSafe.Resource<global::UnityEngine.Shader> SteamVR_Overlay {
+    public static global::TypeSafe.Resource<global::UnityEngine.Shader> UnlitTexture {
         get {
             return ((global::TypeSafe.Resource<global::UnityEngine.Shader>)(__ts_internal_resources[5]));
         }
     }
     
-    public static global::TypeSafe.PrefabResource VivePreColliders {
+    public static global::TypeSafe.Resource<global::UnityEngine.Shader> SteamVR_HiddenArea {
         get {
-            return ((global::TypeSafe.PrefabResource)(__ts_internal_resources[6]));
+            return ((global::TypeSafe.Resource<global::UnityEngine.Shader>)(__ts_internal_resources[6]));
         }
     }
     
-    public static global::TypeSafe.Resource<global::UnityEngine.Shader> SteamVR_Blit {
+    public static global::TypeSafe.Resource<global::UnityEngine.Shader> SteamVR_Overlay {
         get {
             return ((global::TypeSafe.Resource<global::UnityEngine.Shader>)(__ts_internal_resources[7]));
         }
     }
     
-    public static global::TypeSafe.Resource<global::UnityEngine.Shader> SteamVR_AlphaOut {
+    public static global::TypeSafe.PrefabResource VivePreColliders {
         get {
-            return ((global::TypeSafe.Resource<global::UnityEngine.Shader>)(__ts_internal_resources[8]));
+            return ((global::TypeSafe.PrefabResource)(__ts_internal_resources[8]));
         }
     }
     
-    public static global::TypeSafe.Resource<global::UnityEngine.Shader> SteamVR_ColorOut {
+    public static global::TypeSafe.Resource<global::UnityEngine.Shader> SteamVR_Blit {
         get {
             return ((global::TypeSafe.Resource<global::UnityEngine.Shader>)(__ts_internal_resources[9]));
         }
     }
     
+    public static global::TypeSafe.Resource<global::UnityEngine.Shader> SteamVR_AlphaOut {
+        get {
+            return ((global::TypeSafe.Resource<global::UnityEngine.Shader>)(__ts_internal_resources[10]));
+        }
+    }
+    
+    public static global::TypeSafe.Resource<global::UnityEngine.Shader> SteamVR_ColorOut {
+        get {
+            return ((global::TypeSafe.Resource<global::UnityEngine.Shader>)(__ts_internal_resources[11]));
+        }
+    }
+    
+    public static global::TypeSafe.Resource<global::UnityEngine.Shader> SolidColor {
+        get {
+            return ((global::TypeSafe.Resource<global::UnityEngine.Shader>)(__ts_internal_resources[12]));
+        }
+    }
+    
     private static global::System.Collections.Generic.IList<global::TypeSafe.IResource> __ts_internal_resources = new global::System.Collections.ObjectModel.ReadOnlyCollection<global::TypeSafe.IResource>(new global::TypeSafe.IResource[] {
+                new global::TypeSafe.Resource<global::UnityEngine.Shader>("SkyboxMesh", "SkyboxMesh"),
                 new global::TypeSafe.Resource<global::UnityEngine.Shader>("SteamVR_Fade", "SteamVR_Fade"),
                 new global::TypeSafe.Resource<global::UnityEngine.Shader>("SteamVR_BlitFlip", "SteamVR_BlitFlip"),
                 new global::TypeSafe.Resource<global::UnityEngine.Shader>("SteamVR_ClearAll", "SteamVR_ClearAll"),
                 new global::TypeSafe.PrefabResource("SteamVR_ExternalCamera", "SteamVR_ExternalCamera"),
+                new global::TypeSafe.Resource<global::UnityEngine.Shader>("UnlitTexture", "UnlitTexture"),
                 new global::TypeSafe.Resource<global::UnityEngine.Shader>("SteamVR_HiddenArea", "SteamVR_HiddenArea"),
                 new global::TypeSafe.Resource<global::UnityEngine.Shader>("SteamVR_Overlay", "SteamVR_Overlay"),
                 new global::TypeSafe.PrefabResource("VivePreColliders", "VivePreColliders"),
                 new global::TypeSafe.Resource<global::UnityEngine.Shader>("SteamVR_Blit", "SteamVR_Blit"),
                 new global::TypeSafe.Resource<global::UnityEngine.Shader>("SteamVR_AlphaOut", "SteamVR_AlphaOut"),
-                new global::TypeSafe.Resource<global::UnityEngine.Shader>("SteamVR_ColorOut", "SteamVR_ColorOut")});
+                new global::TypeSafe.Resource<global::UnityEngine.Shader>("SteamVR_ColorOut", "SteamVR_ColorOut"),
+                new global::TypeSafe.Resource<global::UnityEngine.Shader>("SolidColor", "SolidColor")});
+    
+    public sealed class UI {
+        
+        private UI() {
+        }
+        
+        public static global::TypeSafe.Resource<global::UnityEngine.Shader> CardboardReticleShader {
+            get {
+                return ((global::TypeSafe.Resource<global::UnityEngine.Shader>)(__ts_internal_resources[0]));
+            }
+        }
+        
+        public static global::TypeSafe.Resource<global::UnityEngine.Material> CardboardReticleMaterial {
+            get {
+                return ((global::TypeSafe.Resource<global::UnityEngine.Material>)(__ts_internal_resources[1]));
+            }
+        }
+        
+        private static global::System.Collections.Generic.IList<global::TypeSafe.IResource> __ts_internal_resources = new global::System.Collections.ObjectModel.ReadOnlyCollection<global::TypeSafe.IResource>(new global::TypeSafe.IResource[] {
+                    new global::TypeSafe.Resource<global::UnityEngine.Shader>("CardboardReticleShader", "UI/CardboardReticleShader"),
+                    new global::TypeSafe.Resource<global::UnityEngine.Material>("CardboardReticleMaterial", "UI/CardboardReticleMaterial")});
+        
+        /// <summary>
+        /// Return a list of all resources in this folder.
+        /// This method has a very low performance cost, no need to cache the result.
+        /// </summary>
+        /// <returns>A list of resource objects in this folder.</returns>
+        public static global::System.Collections.Generic.IList<global::TypeSafe.IResource> GetContents() {
+            return __ts_internal_resources;
+        }
+        
+        private static global::System.Collections.Generic.IList<global::TypeSafe.IResource> __ts_internal_recursiveLookupCache;
+        
+        /// <summary>
+        /// Return a list of all resources in this folder and all sub-folders.
+        /// The result of this method is cached, so subsequent calls will have very low performance cost.
+        /// </summary>
+        /// <returns>A list of resource objects in this folder and sub-folders.</returns>
+        public static global::System.Collections.Generic.IList<global::TypeSafe.IResource> GetContentsRecursive() {
+            if ((__ts_internal_recursiveLookupCache != null)) {
+                return __ts_internal_recursiveLookupCache;
+            }
+            global::System.Collections.Generic.List<global::TypeSafe.IResource> tmp = new global::System.Collections.Generic.List<global::TypeSafe.IResource>();
+            tmp.AddRange(GetContents());
+            __ts_internal_recursiveLookupCache = tmp;
+            return __ts_internal_recursiveLookupCache;
+        }
+        
+        /// <summary>
+        /// Return a list of all resources in this folder of type <typeparamref>TResource</typeparamref> (does not include sub-folders)
+        /// This method does not cache the result, so you should cache the result yourself if you will use it often.
+        /// </summary>
+        /// <returns>A list of <typeparamref>TResource</typeparamref> objects in this folder.</returns>
+        public static global::System.Collections.Generic.List<global::TypeSafe.Resource<TResource>> GetContents<TResource>()
+            where TResource : global::UnityEngine.Object {
+            return global::TypeSafe.TypeSafeUtil.GetResourcesOfType<TResource>(GetContents());
+        }
+        
+        /// <summary>
+        /// Return a list of all resources in this folder of type <typeparamref>TResource</typeparamref>, including sub-folders.
+        /// This method does not cache the result, so you should cache the result yourself if you will use it often.
+        /// </summary>
+        /// <returns>A list of <typeparamref>TResource</typeparamref> objects in this folder and sub-folders.</returns>
+        public static global::System.Collections.Generic.List<global::TypeSafe.Resource<TResource>> GetContentsRecursive<TResource>()
+            where TResource : global::UnityEngine.Object {
+            return global::TypeSafe.TypeSafeUtil.GetResourcesOfType<TResource>(GetContentsRecursive());
+        }
+        
+        /// <summary>
+        /// Call Unload() on every loaded resource in this folder.
+        /// </summary>
+        public static void UnloadAll() {
+            global::TypeSafe.TypeSafeUtil.UnloadAll(GetContents());
+        }
+        
+        /// <summary>
+        /// Call Unload() on every loaded resource in this folder and subfolders.
+        /// </summary>
+        private void UnloadAllRecursive() {
+            global::TypeSafe.TypeSafeUtil.UnloadAll(GetContentsRecursive());
+        }
+    }
     
     /// <summary>
     /// Return a list of all resources in this folder.
@@ -115,6 +218,7 @@ public sealed class SRResources {
         }
         global::System.Collections.Generic.List<global::TypeSafe.IResource> tmp = new global::System.Collections.Generic.List<global::TypeSafe.IResource>();
         tmp.AddRange(GetContents());
+        tmp.AddRange(UI.GetContentsRecursive());
         __ts_internal_recursiveLookupCache = tmp;
         return __ts_internal_recursiveLookupCache;
     }
